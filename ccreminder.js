9ca76aaa539cbba4ea4156179465ba4ad85486f8
tacoder <abhinav.singh21093@gmail.com>
2020-03-25 04:58:34 +0530

    Working code

--- ccreminder.js
+++ ccreminder.js
@@ -1,76 +1,137 @@
-//var config = require('./config.js');
-// /*
-var config =  {
-  bills : [
-      {
-        name: "citibank bill pay",
-        issue_date: 20,
-        due_date: 6
-      },
-      {
-        name: "sbi card bill pay",
-        issue_date: 8,
-        due_date: 28
-      },
-      {
-        name: "amex card bill pay",
-        issue_date: 5,
-        due_date: 23
-      },
-      {
-        name: "mtnl bill pay",
-        issue_date: 8,
-        due_date: 29
-      },
-      {
-        name: "Mother bill pay.",
-        issue_date: 28,
-        due_date: 12
-      },
-      {
-        name: "airtel internet bill pay",
-        issue_date: 27,
-        due_date: 16
-      }
-    ]
-}
-// */
+var config = require('./config.js');
 function runFunc() {
     console.log("Running");
 }
 
+function wasBillPaidBetweenDates(bill, from, to) {
+  return true;
+}
+
+function notifyPending(billConfig, daysLeft) {
+  console.log("Bill payment is pending for ", billConfig.name, " ", daysLeft, " days left to pay");
+}
+
+function notifyUpcoming(billConfig, daysLeft) {
+  console.log("Bill payment is upcoming for ", billConfig.name, " ", daysLeft, " days left to pay");
+}
+
+function notifyGone(billConfig, daysGone) {
+  console.log("Bill payment is gone :( for ", billConfig.name, " ", daysGone, " days ago");
+}
+
 function remindFn() {
+    var todaysDate = new Date().getDay();
     for (bill in config.bills) {
-      remindForBill(config.bills[bill]);
+      remindForBillAndDate(config.bills[bill], todaysDate);
     }
 }
 
-function remindForBill(bill) {
-    console.log("reminding for bill : ", bill["name"], bill);
-    for (var todaysDate = 1 ; todaysDate < 32 ; todaysDate ++){
-        console.log("For day : " , todaysDate);
-        console.log(" isCurrentMonthBill " + isCurrentMonthBill(bill, todaysDate));
-        if (isBillPending(bill, todaysDate)){
-            console.log("Your bill is pending payment!");
-        } else {
-            console.log("RElax! no bill  not pending tofdat:!");
-        }
+function dateBetween(dateToCheck, from, to) {
+  return from <= dateToCheck && dateToCheck < to;
+}
+
+function getDueDate(bill, todaysDate) {
+  if (bill.issue_date <= bill.due_date) {
+    return getCurrentMonthDate(bill.due_date);
+  } else {
+    if(todaysDate < bill.issue_date){
+      return getCurrentMonthDate(bill.due_date)
+    } else {
+      return getNextMonthDate(bill.due_date);
     }
+  }
 }
 
-function isCurrentMonthBill(bill, todaysDate) {
-    return isBillPending(bill, todaysDate)
-    && (bill.issue_date <= bill.due_date)
-         || (bill.issue_date > bill.due_date && todaysDate >= bill.issue_date);
+function getIssuedDate(bill, todaysDate) {
+  if (bill.issue_date <= bill.due_date) {
+    return getCurrentMonthDate(bill.issue_date);
+  } else {
+    if(todaysDate < bill.due_date){
+      return getPreviousMonthDate(bill.issue_date);
+    } else {
+      return getCurrentMonthDate(bill.issue_date);
+    }
+  }
 }
 
-function isBillPending(bill, todaysDate) {
-    return (bill.issue_date <= bill.due_date && dateIsInRange(todaysDate, bill.issue_date, bill.due_date))
-        || (bill.issue_date > bill.due_date && !dateIsInRange(todaysDate, bill.due_date, bill.issue_date));
+function getPreviousMonthDate(todaysDate) {
+  var d =  getCurrentMonthDate(todaysDate);
+  d.setMonth(d.getMonth() - 1 );
+  return d;
 }
 
-function dateIsInRange(dateToCheckFor, start, end) {
-    return dateToCheckFor > start && dateToCheckFor < end;
+function getNextMonthDate(todaysDate) {
+  var d =  getCurrentMonthDate(todaysDate);
+  d.setMonth(d.getMonth() + 1 );
+  return d;
+}
+
+function getKeepRemindingBeforeDate(bill, todaysDate) {
+  var due_date = getDueDate(bill, todaysDate);
+  var krbDate = new Date(due_date);
+  krbDate.setDate(krbDate.getDate() - bill.keep_reminding_before_days);
+  return krbDate
+}
+
+function getCurrentMonthDate(todaysDate) {
+  var d = new Date();
+  d.setDate(todaysDate);
+  return d;
+}
+
+
+function getNotifyAfterDate(bill, todaysDate) {
+  var due_date = getDueDate(bill, todaysDate);
+  var naDate = new Date(due_date);
+  naDate.setDate(naDate.getDate() + bill.notify_after_days);
+  return naDate
+}
+
+function getDateAt (todaysDate ) {
+  var d = new Date();
+  d.setDate(todaysDate);
+  return d;
+}
+function remindForBillAndDate(bill, todaysDate) {
+    var dueDate = getDueDate(bill, todaysDate);
+    var issuedDate = getIssuedDate(bill, todaysDate);
+    var keepRemindingBeforeDate = getKeepRemindingBeforeDate(bill, todaysDate);
+    var notifyAfterDate = getNotifyAfterDate(bill, todaysDate);
+    var today = getDateAt(todaysDate);
+
+    // console.log("getDueDate", dueDate)
+    // console.log("getIssuedDate", issuedDate)
+    // console.log("getKeepRemindingBeforeDate", keepRemindingBeforeDate)
+    // console.log("getNotifyAfterDate", notifyAfterDate)
+    // console.log("today: ", today);
+
+    // Is pending?
+    if (dateBetween(today, issuedDate, dueDate) && !wasBillPaidBetweenDates(bill, issuedDate, dueDate)){
+      notifyPending(bill, Math.round((dueDate - today)/(1000*60*60*24) ));
+    } 
+
+    // Is near?
+    if(dateBetween(today, keepRemindingBeforeDate, dueDate)) {
+     notifyUpcoming(bill, Math.round((dueDate - today)/(1000*60*60*24) ));
+    }
+    
+    // Is recently gone?
+    if(dateBetween(today, dueDate, notifyAfterDate)) {
+     notifyGone(bill,  Math.round((today - dueDate)/(1000*60*60*24)) );
+    }
 }
 
 module.exports = { run : runFunc , remind : remindFn }
+
+if (require.main === module) {
+  for (bill in config.bills) {
+    console.log("==============================");
+    console.log("==============================");
+    console.log("==============================");
+
+    for (var i = 1 ; i <= 31 ; i ++) {
+      console.log("DAY #",i)
+        remindForBillAndDate(config.bills[bill], i);
+    }
+  }
+}
---
+++ config.js
@@ -0,0 +1,47 @@
+var config =  {
+    bills : [
+        {
+          name: "citibank bill pay",
+          issue_date: 20,
+          due_date: 6,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        },
+        {
+          name: "sbi card bill pay",
+          issue_date: 8,
+          due_date: 28,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        },
+        {
+          name: "amex card bill pay",
+          issue_date: 5,
+          due_date: 23,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        },
+        {
+          name: "mtnl bill pay",
+          issue_date: 8,
+          due_date: 29,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        },
+        {
+          name: "Mother bill pay.",
+          issue_date: 28,
+          due_date: 12,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        },
+        {
+          name: "airtel internet bill pay",
+          issue_date: 27,
+          due_date: 16,
+          keep_reminding_before_days: 3,
+          notify_after_days: 2
+        }
+      ]
+  }
+  module.exports = config
