5398d53e9e63abde675a88d5b3a4b2e8aaad07d9
tacoder <abhinav.singh21093@gmail.com>
2020-03-27 02:01:04 +0530

    Adding upload file feature

--- server.js
+++ server.js
@@ -4,13 +4,36 @@ const express = require('express');
 const ccreminder = require('./ccreminder.js');
 const cron = require("node-cron");
 const mongoose = require("./mongo.js");
+const fileUpload = require('express-fileupload');
+const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/var/uploaded-bills/bill-typefilename.jpg', function(err) {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
+  }
+    res.send('File uploaded!');
+  });
+});
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
+// const app = express();
 app.get('/ping', (req, res) => {
   res.send('Hello World Reloaded!');
 });
@@ -25,7 +48,7 @@ app.get('/remind', (req,res) => {
     ccreminder.remind();
 })
 
-cron.schedule(" * * * * *", ccreminder.run);
+cron.schedule("* * * * *", function(){console.log("run")});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
